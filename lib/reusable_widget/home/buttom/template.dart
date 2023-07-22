c7b5b9b9c95f0fb47ea12d21a81e0c7f7123d648
VuxZz <ethnicthv123@gmail.com>
2023-07-22 17:14:14 +0700

    home-screen-buttom 3 <<Hotfix: fix android compatible>> (Ready for Merge)

--- lib/reusable_widget/home/buttom/template.dart
+++ lib/reusable_widget/home/buttom/template.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 
 class Template extends StatelessWidget {
@@ -7,51 +5,59 @@ class Template extends StatelessWidget {
   double? scaleValue;
   final VoidCallback? onPress;
 
-  Template({Key? key, ImageProvider? image, String? text, IconData? icon, Color? inner_color, Color? bg_color, double? width = 100, double? height = 100, this.scaleValue, this.onPress}) : super(key: key) {
-    if(image != null) {
+  Template(
+      {Key? key,
+      ImageProvider? image,
+      String? text,
+      IconData? icon,
+      Color? innerColor,
+      Color? bgColor,
+      double? width = 100,
+      double? height = 100,
+      this.scaleValue,
+      this.onPress})
+      : super(key: key) {
+    if (image != null) {
       child = Container(
         width: width,
         height: height,
         child: Image(image: image),
       );
-    } else if (text != null && child.isUndefinedOrNull) {
+    } else if (text != null && child == null) {
       child = Container(
         width: width,
         height: height,
         child: Center(
           child: Padding(
             padding: EdgeInsets.all(8.0), // Add the desired margin
-            child: Text(text,
-              style: TextStyle(
-                  color: inner_color ?? Colors.blueGrey
-              ),
+            child: Text(
+              text,
+              style: TextStyle(color: innerColor ?? Colors.blueGrey),
             ),
           ),
         ),
         decoration: BoxDecoration(
-          color: bg_color ?? Colors.white, // Set the transparent gray color
-          borderRadius: BorderRadius.circular(6.0), // Set the desired border radius
+          color: bgColor ?? Colors.white, // Set the transparent gray color
+          borderRadius:
+              BorderRadius.circular(6.0), // Set the desired border radius
         ),
       );
-    } else if (icon != null && child.isUndefinedOrNull) {
+    } else if (icon != null && child == null) {
       child = Container(
           width: width,
           height: height,
           child: Center(
             child: Padding(
               padding: EdgeInsets.all(8.0), // Add the desired margin
-              child: Icon(
-                  icon,
-                  color: inner_color ?? Colors.blueGrey
-              ),
+              child: Icon(icon, color: innerColor ?? Colors.blueGrey),
             ),
           ),
           decoration: BoxDecoration(
-            color: bg_color ?? Colors.white, // Set the transparent gray color
-            borderRadius: BorderRadius.circular(6.0), // Set the desired border radius
-          )
-      );
-    } else if (child.isUndefinedOrNull) {
+            color: bgColor ?? Colors.white, // Set the transparent gray color
+            borderRadius:
+                BorderRadius.circular(6.0), // Set the desired border radius
+          ));
+    } else if (child == null) {
       child = Container(
           width: width,
           height: height,
@@ -60,26 +66,26 @@ class Template extends StatelessWidget {
               padding: EdgeInsets.all(8.0), // Add the desired margin
               child: Icon(
                 Icons.access_alarm,
-                color: inner_color ?? Colors.blueGrey,
+                color: innerColor ?? Colors.blueGrey,
               ),
             ),
           ),
           decoration: BoxDecoration(
-            color: bg_color ?? Colors.white, // Set the transparent gray color
-            borderRadius: BorderRadius.circular(6.0), // Set the desired border radius
-          )
-      );
+            color: bgColor ?? Colors.white, // Set the transparent gray color
+            borderRadius:
+                BorderRadius.circular(6.0), // Set the desired border radius
+          ));
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TemplateButton(
-        onPressed: onPress ?? () {
-          print("press");
-        },
+        onPressed: onPress ??
+            () {
+              print("press");
+            },
         child: child!,
         scaleValue: scaleValue ?? 1.5,
       ),
@@ -91,7 +97,8 @@ class TemplateButton extends StatefulWidget {
   final VoidCallback onPressed;
   final Widget child;
   final double scaleValue;
-  TemplateButton({required this.onPressed, required this.child, required this.scaleValue});
+  TemplateButton(
+      {required this.onPressed, required this.child, required this.scaleValue});
 
   @override
   _TemplateButtonState createState() => _TemplateButtonState();
@@ -99,7 +106,6 @@ class TemplateButton extends StatefulWidget {
 
 class _TemplateButtonState extends State<TemplateButton>
     with SingleTickerProviderStateMixin {
-
   late AnimationController _animationController;
 
   _TemplateButtonState();
@@ -148,6 +154,3 @@ class _TemplateButtonState extends State<TemplateButton>
     );
   }
 }
-
-
-
