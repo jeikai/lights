a1110b07d28a4d9158bc68806ff0eca6aeaf95f1
VuxZz <ethnicthv123@gmail.com>
2023-07-25 23:16:32 +0700

    list-view 4 <<NormalCell of Noti Widget>>

---
+++ lib/reusable_widget/vux/listview/noti/normal_cell.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/reusable_widget/avatar.dart';
+import 'package:flutterapp/util/rive/RiveUtil.dart';
+import 'package:rive/rive.dart';
+
+class NormalCell extends StatelessWidget {
+  const NormalCell({Key? key, this.isClickable = false, this.onPress}) : super(key: key);
+
+  final bool isClickable;
+
+  final VoidCallback? onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context2, constr) {
+      double w_l = constr.maxWidth / 6;
+      double w_r = constr.maxWidth / 6 * 3.9;
+      return Container(
+        height: w_l,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 250,241,255),
+          borderRadius: BorderRadius.circular(w_l / 4),
+          border: Border.all(),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: w_l,
+              child: Center(
+                child: Avatar(
+                  size: w_l / 2,
+                ),
+              ),
+            ),
+            Container(
+              width: w_r,
+              child: Center(
+
+              ),
+            ),
+            Builder(builder: (context2) {
+              return isClickable ? Container(
+                width: w_l,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      onPress?.call();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                    ),
+                    child: RiveAnimation.direct(RiveUtil().getStar()),
+                  ),
+                ),
+              ) : Container();
+            })
+          ],
+        ),
+      );
+    });
+  }
+}
--- test/test.dart
+++ test/test.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/reusable_widget/vux/listview/dailytask/daily_task.dart';
+import 'package:flutterapp/reusable_widget/vux/listview/noti/normal_cell.dart';
 import 'package:flutterapp/setting.dart';
+import 'package:flutterapp/util/rive/RiveUtil.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DataCaching());
+}
+
+class DataCaching extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DataCachingState();
+}
+
+class DataCachingState extends State<DataCaching> {
+  RiveUtil? riveUtil;
+
+  @override
+  void initState() {
+    super.initState();
+    riveUtil = RiveUtil();
+    riveUtil!.setup().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+
 }
 
 class MyApp extends StatelessWidget {
@@ -11,16 +36,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     setting.setWidthSize(MediaQuery.of(context).size.width);
     setting.setHeightSize(MediaQuery.of(context).size.height);
-
-    print(setting.heightSize == null);
-    print(setting.widthSize == null);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Template Button Example')),
         body: Container(
           color: Colors.red,
           child: Center(
-            child: DailyTaskProxy()
+            child: NormalCell(
+              isClickable: true,
+              onPress: () {
+                print("press");
+              },
+            )
           ),
         )
       ),
