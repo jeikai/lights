c5b80453c8f09eb8b2581075198428555197cd94
jeikai <98737616+jeikai@users.noreply.github.com>
2024-07-14 10:07:46 +0700

    hi

--- server/models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const favoriteSchema = new mongoose.Schema({
-  description: { type: String },
-});
-
-const Favorite = mongoose.model('Favorite', favoriteSchema);
- 
--- server/models/Favorite_User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const favoriteUserSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  favoriteId: { type: mongoose.Schema.Types.ObjectId, ref: 'Favorite' },
-});
-
-const Favorite_User = mongoose.model('Favorite_User', favoriteUserSchema);
